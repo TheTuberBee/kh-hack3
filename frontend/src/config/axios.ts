3b9c0bc35c9ad8cc12ef622844d43213bc581326
Zsombor Horvath <horvathzsombor2005@gmail.com>
2023-02-25 12:03:33 +0100

    Change axios url

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL =
-  process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
+  process.env.REACT_APP_BACKEND_URL ||
+  "https://kh-hack3-pb0qz3ela-thetuberbee.vercel.app";
 
 export default axios;
