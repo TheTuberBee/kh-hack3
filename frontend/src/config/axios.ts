60ab458eb0c86eaa33b1e3152b61a6fc9d8e354c
Zsombor Horvath <horvathzsombor2005@gmail.com>
2023-02-25 08:08:27 +0100

    Add new chart to profile page

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
+import process from "process";
 
-axios.defaults.baseURL = "http://localhost:8080";
+axios.defaults.baseURL =
+  process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
 
 export default axios;
